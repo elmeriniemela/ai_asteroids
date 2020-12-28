1d815753245c46e8f171a2a7704ea2119e2124dc
Elmeri Niemelä <niemela.elmeri@gmail.com>
2020-12-28 21:22:25 +0200

    Separate speed and direction from velocity to allow better player movement

--- ball.py
+++ ball.py
@@ -10,36 +10,66 @@ import colors as C
 
 class Game:
     def __init__(self, width, height):
-        self.window_size = width, height
-        self.window = pygame.display.set_mode(self.window_size)
+        self.width, self.height = width, height
+        self.window = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Asteroids")
         self.surface = pygame.Surface([width, height])
         self.surface.fill(C.white)
 
     def run(self):
         running = True
-        spritelist = [Asteroid.random(*self.window_size) for _ in range(10)]
-        group = pygame.sprite.Group(spritelist)
+        spritelist = [Asteroid.random(self.width, self.height) for _ in range(10)]
+        asteroids = pygame.sprite.Group(spritelist)
+        player = Player(
+            pos=[self.width/2, self.height/2],
+            direction=[0,-1],
+            speed=0,
+            width=30,
+            height=60,
+        )
+
+        p_group = pygame.sprite.Group(player)
 
         clock = pygame.time.Clock()
         fps = 60
+        rotate_speed = 1.5
 
         while running is True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        player.thrust = True
+                    if event.key == pygame.K_LEFT:
+                        player.rotate_speed = -rotate_speed
+                    if event.key == pygame.K_RIGHT:
+                        player.rotate_speed = rotate_speed
+                    if event.key == pygame.K_SPACE and player_dying_delay == 0 and len(bullets) < bullet_capacity:
+                        bullets.append(Bullet(player.x, player.y, player.dir))
+                    if event.key == pygame.K_LSHIFT:
+                        hyperspace = 30
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        player.thrust = False
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                        player.rotate_speed = 0
+
             dt = clock.tick(fps)
 
-            for a, b in combinations(group, r=2):
+            for a, b in combinations(asteroids, r=2):
                 if pygame.sprite.collide_circle(a, b):
                     a.collide(b)
 
             for ball in spritelist:
                 ball.update(dt)
 
+            player.update(dt)
+
             game.window.blit(game.surface, [0, 0])
-            group.draw(game.window)
+            asteroids.draw(game.window)
+            p_group.draw(game.window)
 
             pygame.display.update()
 
@@ -47,15 +77,26 @@ class Game:
 
 class Object(pygame.sprite.Sprite):
 
-    def __init__(self, pos, speed, width, height):
+    def __init__(self, pos, direction, speed, width, height):
         super().__init__()
         self.image = pygame.Surface([width, height])
         self.image.fill(C.white)
         self.image.set_colorkey(C.white)
         self.rect = self.image.get_rect()
         self.position = pos
-        self.speed = vector.Vector(*speed)
+        self.direction = direction
+        self.speed = speed
 
+
+    @property
+    def velocity(self):
+        return vector.Vector(*self.direction) * self.speed
+
+    @velocity.setter
+    def velocity(self, value):
+        assert isinstance(value, vector.Vector)
+        self.speed = value.norm()
+        self.direction = value.normalize()
 
     @property
     def position(self):
@@ -84,20 +125,19 @@ class Object(pygame.sprite.Sprite):
         self.position = self.x, value
 
     def move(self, dt):
-        self.position += self.speed * dt
+        self.position += self.velocity * dt
 
     def wall_collision(self):
-        max_width, max_height = game.window_size
         width, height = self.image.get_width(), self.image.get_height()
-        if self.x > max_width - (width / 2):
+        if self.x > game.width - (width / 2):
             self.x = -(width / 2)
         elif self.x < -(width / 2):
-            self.x = max_width - (width / 2)
+            self.x = game.width - (width / 2)
 
-        if self.y > max_height - (height / 2):
+        if self.y > game.height - (height / 2):
             self.y = -(height / 2)
         elif self.y < -(height / 2):
-            self.y = max_height - (height / 2)
+            self.y = game.height - (height / 2)
 
     def update(self, dt):
         self.wall_collision()
@@ -105,15 +145,42 @@ class Object(pygame.sprite.Sprite):
 
 
 class Player(Object):
-    pass
+    _max_speed = 0.7
+    _acceleration = 0.0005
 
+    def __init__(self, pos, direction, speed, width, height):
+        super().__init__(pos, direction, speed, width, height)
+        line_width = 6
+        pygame.draw.polygon(
+            surface=self.image,
+            color=C.black,
+            points=[
+                (line_width, height-line_width),
+                (width/2, line_width),
+                (width-line_width, height-line_width)
+            ],
+            width=line_width,
+        )
+
+        self.thrust = False
+        self.rotate_speed = 0.0
+
+
+
+    def move(self, dt):
+        super().move(dt)
+        if self.thrust and self.velocity.norm() < self._max_speed:
+            self.speed += (self._acceleration * dt)
+
+        if self.rotate_speed:
+            self.image = pygame.transform.rotate(self.image, self.rotate_speed)
 
 
 class Asteroid(Object):
     _draw_boxes = False
 
-    def __init__(self, pos, speed, radius, mass, color=C.red):
-        super().__init__(pos, speed, radius*2, radius*2)
+    def __init__(self, pos, direction, speed, radius, mass, color=C.red):
+        super().__init__(pos, direction, speed, radius*2, radius*2)
         pygame.draw.circle(self.image, color, (radius, radius), radius)
         if self._draw_boxes:
             N = 3
@@ -129,7 +196,7 @@ class Asteroid(Object):
 
                     pygame.draw.lines(
                         surface=self.image,
-                        color=black,
+                        color=C.black,
                         closed=True,
                         points=lines,
                         width=3,
@@ -141,10 +208,11 @@ class Asteroid(Object):
     @classmethod
     def random(cls, width, height):
         mass = random.choice([100.0,90.0,80.0,70.0])
-        speed_limit = 0.5
+        direction = [random.uniform(-1, 1), random.uniform(-1, 1)]
         return cls(
             pos=[random.randint(0, width), random.randint(0, height)],
-            speed=[random.uniform(-speed_limit, speed_limit), random.uniform(-speed_limit, speed_limit)],
+            direction=direction,
+            speed=random.uniform(0.0, 0.5),
             radius=mass / 2,
             mass=mass
         )
@@ -160,25 +228,24 @@ class Asteroid(Object):
         self.position += normal
         other.position -= normal
 
-        v1n = normal * self.speed
-        v2n = normal * other.speed
-        v1t = tangent * self.speed
-        v2t = tangent * other.speed
+        v1n = normal * self.velocity
+        v2n = normal * other.velocity
+        v1t = tangent * self.velocity
+        v2t = tangent * other.velocity
 
-        # Calculate new speed scalars on the collision normal
+        # Calculate new velocity scalars on the collision normal
         normal_speed1 = normal * (v1n * (self.mass - other.mass) + 2 * other.mass * v2n) / (self.mass + other.mass)
         normal_speed2 = normal * (v2n * (other.mass - self.mass) + 2 * self.mass * v1n) / (self.mass + other.mass)
 
-        # Tangent speed stays the same
+        # Tangent velocity stays the same
         tangent_speed1 = v1t * tangent
         tangent_speed2 = v2t * tangent
 
-        self.speed = normal_speed1 + tangent_speed1
-        other.speed = normal_speed2 + tangent_speed2
+        self.velocity = normal_speed1 + tangent_speed1
+        other.velocity = normal_speed2 + tangent_speed2
 
 
 if __name__ == "__main__":
-    window_size = (640*2, 480*2)
-    game = Game(*window_size)
+    game = Game(640*2, 480*2)
     game.run()
 
