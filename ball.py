896519be650a79b41c9c95e5a00ea4c881f03151
Elmeri Niemelä <niemela.elmeri@gmail.com>
2020-12-28 15:27:55 +0200

    Refactor, add Vector class, make collision part of ball class, wrap around edges

--- ball.py
+++ ball.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+from itertools import combinations
+
 import vector
 from colors import *
 
@@ -16,92 +19,101 @@ class Ball(pygame.sprite.Sprite):
         self.image = pygame.Surface([radius * 2, radius * 2])
         self.image.fill(white)
         self.image.set_colorkey(white)
-        pygame.draw.circle(self.image, color, (radius, radius), radius, )
-        self.origin = pos[0] + radius, pos[1] + radius
-        self.pos = pos
+        pygame.draw.circle(self.image, color, (radius, radius), radius, width=1)
+        N = 3
+        step = (radius*2)/N
+        for y in range(N):
+            for x in range(N):
+                lines = [
+                    (x*step, y*step),
+                    ((x+1)*step, y*step),
+                    ((x+1)*step, (y+1)*step),
+                    (x*step, (y+1)*step),
+                ]
+
+                pygame.draw.lines(
+                    surface=self.image,
+                    color=black,
+                    closed=True,
+                    points=lines,
+                    width=3,
+                )
+
         self.rect = self.image.get_rect()
         self.radius = radius
-        self.speed = speed
-        self.set_position(pos)
+        self.speed = vector.Vector(*speed)
+        self.position = pos
         self.mass = mass
-        print("init")
 
-    def set_position(self, pos):
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
-        self.origin = pos[0] + self.radius, pos[1] + self.radius
-        self.pos = pos
+
+    @property
+    def origin(self):
+        return vector.Vector(self.rect.x + self.radius, self.rect.y + self.radius)
+
+    @property
+    def position(self):
+        return vector.Vector(self.rect.x, self.rect.y)
+
+    @position.setter
+    def position(self, value):
+        self.rect.x, self.rect.y = value
 
     def move(self):
-        self.pos = vector.plus(self.pos, self.speed)
-        self.set_position(self.pos)
+        self.rect.x, self.rect.y = self.position + self.speed
 
     def wall_collision(self):
+        max_width, max_height = game.window_size
+        width, height = self.image.get_width(), self.image.get_height()
 
-        if self.pos[0] > game.window_size[0] - self.image.get_width():
-            self.pos[0] = game.window_size[0] - self.image.get_width()
-            self.speed[0] = -self.speed[0]
+        if self.rect.x > max_width - (width / 2):
+            self.rect.x = -(width / 2)
+        elif self.rect.x < -(width / 2):
+            self.rect.x = max_width - (width / 2)
 
-        if self.pos[0] < 0:
-            self.pos[0] = 0
-            self.speed[0] = -self.speed[0]
-
-        if self.pos[1] > game.window_size[1] - self.image.get_height():
-            self.pos[1] = game.window_size[1] - self.image.get_height()
-            self.speed[1] = -self.speed[1]
-
-        if self.pos[1] < 0:
-            self.pos[1] = 0
-            self.speed[1] = -self.speed[1]
+        if self.rect.y > max_height - (height / 2):
+            self.rect.y = -(height / 2)
+        elif self.rect.y < -(height / 2):
+            self.rect.y = max_height - (height / 2)
 
     def update(self):
         self.wall_collision()
         self.move()
 
+    def collide(self, other):
+        normal = (self.origin - other.origin).normalize()
+        tangent = normal.perpendicular()
 
-def object_collision(sprite1, sprite2):
-    normal = [sprite1.origin[0] - sprite2.origin[0], sprite1.origin[1] - sprite2.origin[1]]
-    unit_normal = vector.unit(normal)
+        self.position += normal
+        other.position -= normal
 
-    unit_tangent = [-unit_normal[1], unit_normal[0]]
-    neg_unit_normal = vector.times(-1, unit_normal)
+        v1n = normal * self.speed
+        v2n = normal * other.speed
+        v1t = tangent * self.speed
+        v2t = tangent * other.speed
 
-    sprite1.pos = vector.plus(sprite1.pos, unit_normal)
-    sprite1.set_position(sprite1.pos)
+        # Calculate new speed scalars on the collision normal
+        normal_speed1 = normal * (v1n * (self.mass - other.mass) + 2 * other.mass * v2n) / (self.mass + other.mass)
+        normal_speed2 = normal * (v2n * (other.mass - self.mass) + 2 * self.mass * v1n) / (self.mass + other.mass)
 
-    sprite2.pos = vector.plus(sprite2.pos, neg_unit_normal)
-    sprite2.set_position(sprite2.pos)
+        # Tangent speed stays the same
+        tangent_speed1 = v1t * tangent
+        tangent_speed2 = v2t * tangent
 
-    v1n = vector.dotp(unit_normal, sprite1.speed)
+        self.speed = normal_speed1 + tangent_speed1
+        other.speed = normal_speed2 + tangent_speed2
 
-    v1t = vector.dotp(unit_tangent, sprite1.speed)
 
-    v2n = vector.dotp(unit_normal, sprite2.speed)
 
-    v2t = vector.dotp(unit_tangent, sprite2.speed)
-
-    u1n = (v1n * (sprite1.mass - sprite2.mass) + 2 * sprite2.mass * v2n) / (sprite1.mass + sprite2.mass)
-
-    u2n = (v2n * (sprite2.mass - sprite1.mass) + 2 * sprite1.mass * v1n) / (sprite1.mass + sprite2.mass)
-
-    sprite1_tangent = vector.times(v1t, unit_tangent)
-    sprite1_normal = vector.times(u1n, unit_normal)
-
-    sprite2_tangent = vector.times(v2t, unit_tangent)
-    sprite2_normal = vector.times(u2n, unit_normal)
-
-    sprite1.speed = vector.plus(sprite1_tangent, sprite1_normal)
-    sprite2.speed = vector.plus(sprite2_tangent, sprite2_normal)
 
 if __name__ == "__main__":
 
     game = Game(640, 480)
     running = True
-    ball01 = Ball(pos=[0, 0], speed=[1, 1], radius=50, mass=500, color=blue)
-    ball02 = Ball(pos=[0, 430], speed=[0, 0], radius=50, mass=10, color=red)
-    ball03 = Ball(pos=[430, 0], speed=[2, 2], radius=25, mass=5, color=black)
-    ball04 = Ball(pos=[330, 0], speed=[2, 2], radius=25, mass=5, color=green)
-    ball05 = Ball(pos=[230, 0], speed=[2, 2], radius=25, mass=5, color=gray)
+    ball01 = Ball(pos=[0, 0], speed=[1, 1], radius=30, mass=5, color=blue)
+    ball02 = Ball(pos=[0, 430], speed=[0, 0], radius=30, mass=5, color=red)
+    ball03 = Ball(pos=[430, 0], speed=[2, 2], radius=30, mass=5, color=black)
+    ball04 = Ball(pos=[330, 0], speed=[2, 2], radius=30, mass=5, color=green)
+    ball05 = Ball(pos=[230, 0], speed=[2, 2], radius=30, mass=5, color=gray)
 
     spritelist = [ball01, ball02, ball03, ball04, ball05]
 
@@ -109,21 +121,16 @@ if __name__ == "__main__":
 
     clock = pygame.time.Clock()
     fps = 120
-    # This is so that collision loop doesnt call object_collision twice per collision(there is probably a better way)
-    count = 0
-    active = 0
 
     while running is True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        for a in group:
-            for b in group:
-                if a != b and pygame.sprite.collide_circle(a, b) and count == 0:
-                    object_collision(a, b)
-                    count = 1
-        count = 0
+
+        for a, b in combinations(group, r=2):
+            if pygame.sprite.collide_circle(a, b):
+                a.collide(b)
 
         for ball in spritelist:
             ball.update()
